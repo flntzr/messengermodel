780e22e62808a7a6fd111258efce65cca3f05e8e
Jakob <pfeiffer.jakob@gmx.de>
2017-10-19 16:38:58 +0200

    updated BaseEntity

--- src/BaseEntity.java
+++ src/BaseEntity.java
@@ -9,8 +9,44 @@ public class BaseEntity implements Comparable{
     private long creationTimestamp;
     List<Message> messagesCaused;
 
+    protected BaseEntity(){
+
+    }
+
+    public long getIdentity() {
+        return identity;
+    }
+
+    public void setIdentity(long identity) {
+        this.identity = identity;
+    }
+
+    public int getVersion() {
+        return version;
+    }
+
+    public void setVersion(int version) {
+        this.version = version;
+    }
+
+    public long getCreationTimestamp() {
+        return creationTimestamp;
+    }
+
+    public void setCreationTimestamp(long creationTimestamp) {
+        this.creationTimestamp = creationTimestamp;
+    }
+
+    public List<Message> getMessagesCaused() {
+        return messagesCaused;
+    }
+
+    public void setMessagesCaused(List<Message> messagesCaused) {
+        this.messagesCaused = messagesCaused;
+    }
+
     @Override
     public int compareTo(Object o) {
-        return 0;
+        return this.identity < ((BaseEntity) o).identity ? -1 : this.identity > ((BaseEntity) o).identity ? 1 : 0;
     }
 }
